421a0fdc62897429593ab88a62f0886b4de6a6c6
Swapnil Banga <swapnil.galaxy@gmail.com>
2020-06-20 23:58:13 +0530

    add view books functionality and add book component

---
+++ src/components/AddBook.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { getAuthorsQuery } from '../queries/queries';
+
+class AddBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      genre: '',
+      authorId: '',
+    };
+  }
+
+  displayAuthors() {
+    const data = this.props.data;
+    if (data.loading) {
+      return <option>Loading authors</option>;
+    }
+    return data.authors.map((author) => {
+      return (
+        <option key={author.id} value={author.id}>
+          {author.name}
+        </option>
+      );
+    });
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form id="add-book" onSubmit={this.submitForm.bind(this)}>
+        <div className="field">
+          <label>Book name:</label>
+          <input
+            type="text"
+            onChange={(e) => this.setState({ name: e.target.value })}
+          />
+        </div>
+        <div className="field">
+          <label>Genre:</label>
+          <input
+            type="text"
+            onChange={(e) => this.setState({ genre: e.target.value })}
+          />
+        </div>
+        <div className="field">
+          <label>Author:</label>
+          <select onChange={(e) => this.setState({ authorId: e.target.value })}>
+            <option>Select author</option>
+            {this.displayAuthors()}
+          </select>
+        </div>
+        <button>+</button>
+      </form>
+    );
+  }
+}
+
+export default graphql(getAuthorsQuery)(AddBook);
